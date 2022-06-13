7527cbb2cea1ecb550162b70a2f1b61a412cb990
anju1140 <anju114@outlook.com>
2022-06-13 12:25:47 +0530

    'success'

---
+++ src/main/java/com/example/dockerjenkinsintegrate/DockerJenkinsIntegrateApplication.java
@@ -0,0 +1,18 @@
+package com.example.dockerjenkinsintegrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerJenkinsIntegrateApplication {
+	@GetMapping
+	public String message(){
+		return  "welcome to devops integration";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrateApplication.class, args);
+	}
+}
